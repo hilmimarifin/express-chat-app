57e71ec0fe4c9b536c766517b7f8ab013defc30b
hilmimarifin <hilmi.m.arifin@gmail.com>
2022-12-11 16:45:21 +0700

    add input validation on sign up user

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import RoleController from "../controllers/RoleController";
 import UserController from "../controllers/UserController";
+import UserValidation from "../middleware/validation/UserValidation";
 
 const router = express.Router();
 
@@ -13,6 +14,6 @@ router.delete("/role/:id", RoleController.DeleteRole);
 router.get("/role/:id", RoleController.GetRoleById);
 
 //user router
-router.post("/user/signup", UserController.Register);
+router.post("/user/signup",UserValidation.RegisterValidation, UserController.Register);
 
 export default router;
