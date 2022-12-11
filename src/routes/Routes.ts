070e79fe9769a451f9772d0b10aa7e52a8e9bcd2
hilmimarifin <hilmi.m.arifin@gmail.com>
2022-12-11 16:31:31 +0700

    Add User signup API

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,14 +1,18 @@
 import express from "express";
 
 import RoleController from "../controllers/RoleController";
+import UserController from "../controllers/UserController";
 
 const router = express.Router();
 
+//role router
 router.get("/role",  RoleController.GetRole);
 router.post("/role", RoleController.CreateRole);
 router.post("/role/:id", RoleController.UpdateRole);
 router.delete("/role/:id", RoleController.DeleteRole);
 router.get("/role/:id", RoleController.GetRoleById);
 
+//user router
+router.post("/user/signup", UserController.Register);
 
 export default router;
