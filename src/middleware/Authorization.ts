7c2a1166258f361c012c6eeda79e01e18298d46c
hilmimarifin <hilmi.m.arifin@gmail.com>
2022-12-12 14:33:19 +0700

    Add API auth based on role middleware

--- src/middleware/Authorization.ts
+++ src/middleware/Authorization.ts
@@ -21,4 +21,43 @@ const Authenticated = (req: Request, res: Response, next: NextFunction) => {
 	}
 }
 
-export default { Authenticated }
+const SuperUser = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const roleId = res.locals.roleId;
+		if (roleId !== 1) {
+			return res.status(401).send(Helper.ResponseData(403, "Forbidden", null, null));
+		}
+
+		next();
+	} catch (err:any) {
+		return res.status(500).send(Helper.ResponseData(500, "", err, null));
+	}
+};
+
+const AdminRole = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const roleId = res.locals.roleId;
+		if (roleId !== 2) {
+			return res.status(401).send(Helper.ResponseData(403, "Forbidden", null, null));
+		}
+
+		next();
+	} catch (err:any) {
+		return res.status(500).send(Helper.ResponseData(500, "", err, null));
+	}
+};
+
+const BasicUser = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const roleId = res.locals.roleId;
+		if (roleId !== 3) {
+			return res.status(401).send(Helper.ResponseData(403, "Forbidden", null, null));
+		}
+
+		next();
+	} catch (err:any) {
+		return res.status(500).send(Helper.ResponseData(500, "", err, null));
+	}
+};
+
+export default { Authenticated, SuperUser, AdminRole, BasicUser }
