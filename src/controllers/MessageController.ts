68a345bb2f8a0d7d33e220565ebd673e75f396ba
Hilmi <hilmi.m.arifin@gmail.com>
2023-01-04 16:57:21 +0700

    fix displaying last message on list messages

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response, raw } from "express";
 import Message from "../db/models/Message";
 import Helper from "../helpers/Helper";
-import { Op } from "sequelize"
+import { Op, Sequelize } from "sequelize"
 
 const CreateMessage = async (req: Request, res: Response): Promise<Response> => {
     try {
@@ -35,13 +35,23 @@ const GetDetailMessage = async (req: Request, res: Response): Promise<Response> 
         const decodedUser = Helper.ExtractRefreshToken(refreshToken);
         const senderId = decodedUser?.id
 
-        const message = await Message.findAll({
+        const messages = await Message.findAll({
+            where: {
+                [Op.or]: [{ receiverId, senderId }, { receiverId: senderId, senderId: receiverId }],
+            }
+        })
+        const ids = messages.map(message => message.id)
+
+        await Message.update({ hasSeen: true }, { where: { id: ids } })
+
+        const updatedMessages = await Message.findAll({
             where: {
                 [Op.or]: [{ receiverId, senderId }, { receiverId: senderId, senderId: receiverId }],
             }
         })
 
-        return res.status(201).send(Helper.ResponseData(201, "Success", null, message));
+
+        return res.status(201).send(Helper.ResponseData(201, "Success", null, updatedMessages));
     } catch (error: any) {
         return res.status(500).send(Helper.ResponseData(500, "", error, null));
     }
@@ -50,7 +60,6 @@ const GetDetailMessage = async (req: Request, res: Response): Promise<Response> 
 const GetListMessage = async (req: Request, res: Response): Promise<Response> => {
     try {
         //todos
-        //1. Order for last message to show in the list
         //2. unread count
         const refreshToken = req.cookies?.refreshToken
 
@@ -60,16 +69,53 @@ const GetListMessage = async (req: Request, res: Response): Promise<Response> =>
 
         const decodedUser = Helper.ExtractRefreshToken(refreshToken);
         const senderId = decodedUser?.id
-
-        const message = await Message.findAll({
+        const messages = await Message.findAll({
+            attributes: ["id", [Sequelize.fn('MAX', Sequelize.col('text')), 'text'], "senderId", "receiverId", "createdAt"],
             where: {
                 [Op.or]: [{ receiverId: senderId }, { senderId }]
             },
-            order: [['createdAt', 'ASC']],
-            group: 'receiverId'
+            group: ["receiverId", "senderId"],
+            raw: true
         })
+        const combineMessage = (messagesParams: any) => {
+            const combined: any = []
+            messagesParams.forEach((message: any) => {
+                messagesParams.forEach((matcher: any) => {
+                    if (message.senderId === matcher.receiverId && message.receiverId === matcher.senderId) {
+                        combined.push([message, matcher])
+                    }
+                })
+            })
+            return combined
+        }
+        const getLatestMessage = (messageParams: any[]) => {
+            return messageParams.map((message: any[]) => {
+                return message.reduce((result: any, current: any) => {
+                    const da = new Date(result?.createdAt)
+                    const db = new Date(current?.createdAt)
+                    return (da > db ? result : current)
+                })
+            })
+        }
+        const removeDuplicateMessage = (messageParams: any[]) => {
+            return messageParams.reduce((result: any[], current) => {
+                let found = false;
+                for (let i = 0; i < result.length; i++) {
+                   if (result[i].id === current.id) {
+                      found = true;
+                   };
+                }
+                if (!found) {
+                   result.push(current);
+                }
+                return result;
+            }, [])
+        }
 
-        return res.status(201).send(Helper.ResponseData(201, "Success", null, message));
+        const combinedMessage = combineMessage(messages)
+        const latestMessage = getLatestMessage(combinedMessage)
+        const finalMessage = removeDuplicateMessage(latestMessage)
+        return res.status(201).send(Helper.ResponseData(201, "Success", null, finalMessage));
     } catch (error: any) {
         return res.status(500).send(Helper.ResponseData(500, "", error, null));
     }
--- src/helpers/Helper.ts
+++ src/helpers/Helper.ts
@@ -33,7 +33,7 @@ const ResponseData = (status: number, message: string | null, error: any | null,
 };
 
 const GenerateToken = (data: any): string => {
-	const token = jwt.sign(data, process.env.JWT_TOKEN as string, { expiresIn: "10m" });
+	const token = jwt.sign(data, process.env.JWT_TOKEN as string, { expiresIn: "60m" });
 
 	return token;
 }
